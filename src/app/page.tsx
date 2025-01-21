e9ce5458001fc16dad5aa6c97fd3b2754c0f807b
Erik Ungvari <eroplas47@gmail.com>
2025-01-21 11:28:16 +0100

    test

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <p>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmNlzHnCNURLtqczpWEpYtkSi1n46v92dP7A&s"></img>
-    </p>
-  );
-}
---
+++ src/app/registration.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="container md bg-white">
+          d
+      </div>
+    </div>
+  );
+}
