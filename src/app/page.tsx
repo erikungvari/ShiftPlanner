518ffdfe972e66ca6079b3bfa2bb93cf83321fb4
Erik Ungvari <eroplas47@gmail.com>
2025-01-21 10:53:40 +0100

    trying to get this to fckn work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <p>
     <img src="https://preview.redd.it/hey-southern-parts-of-our-empire-bober-is-watching-over-you-v0-mywwqkt42nod1.jpeg?width=640&crop=smart&auto=webp&s=2b4dbe0e5eae5f663ba58230504c33ce1c963f4b"></img>
+    </p>
   );
 }
